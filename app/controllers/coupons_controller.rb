de0167d4182eaea07d55cd0f2cc7e4da181d6e6d
Ikaquan Bessee <ikaquanbessee@yahoo.com>
2021-03-15 22:54:22 +0000

    Automatically backed up by Learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -11,7 +11,6 @@ class CouponsController < ApplicationController
   end
 
   def create
-    byebug
     @coupon = Coupon.new
     @coupon.coupon_code = params[:coupon_code]
     @coupon.store = params[:store]
